13b97c82ac717fb45fce34725100a4492f6c2dbb
Wagner Rodrigues <wagnersr92@gmail.com>
2021-03-20 11:32:48 -0300

    Criação da rota categories e add de lib uuid

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 
+// Quando não é export default, precisa colocar entre chaves
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running"));
