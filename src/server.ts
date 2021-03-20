77f0d5a18abc76c902a112dd1547a28d9c41a096
Wagner Rodrigues <wagnersr92@gmail.com>
2021-03-20 11:12:34 -0300

    Ajuste p/ debug e leitura de json

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" });
-});
+app.use(express.json());
 
 app.listen(3333, () => console.log("Server is running"));
