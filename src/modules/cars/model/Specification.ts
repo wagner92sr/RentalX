2d3993287913dc677c000f9bd168f1156163c02e
Wagner Rodrigues <wagnersr92@gmail.com>
2021-03-21 21:59:13 -0300

    Ajuste nas pastas e criação da Specification

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidV4 } from "uuid"; // cria alias uuidv4 para v4
+
+class Specification {
+    id?: string;
+    name: string;
+    description: string;
+    created_at: Date;
+    /** Se o id estiver nulo, gera um novo
+     * Se veio populado(é atualizção) utiliza o recebido
+     */
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { Specification };
