09b5cf1ad19feed88bd528c355d5db150a793602
Wagner Rodrigues <wagnersr92@gmail.com>
2021-03-23 19:18:15 -0300

    refatoração de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
 
-// Quando não é export default, precisa colocar entre chaves
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
