5aa242930b6d5e4ae547e982f339b30f963d4a2a
Wagner Rodrigues <wagnersr92@gmail.com>
2021-03-23 18:47:20 -0300

    Adequação ao padrão singleton

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,8 +7,21 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
+    }
+
+    /**
+     * Singleton getInstance sera responsavel por criar ou retornar instancia
+     * existente. Evitando a criacao de mais de uma instancia.
+     */
+    public static getInstance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ description, name }: ICreateCategoryDTO): void {
